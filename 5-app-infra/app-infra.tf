2ea84c1104f1edd42558c0dd50b8cc2b49238314
Rahul Shivalkar <rahulshivalkar@rediffmail.com>
2025-08-06 14:46:34 +0530

    fix acl and policy

--- 5-app-infra/app-infra.tf
+++ 5-app-infra/app-infra.tf
@@ -23,27 +23,27 @@ resource "aws_s3_bucket_website_configuration" "frontend_ui_website" {
   }
 }
 
-# Block all public access by default, but allow specific public access via policy
+# This resource ensures that the public access is managed by a bucket policy
 resource "aws_s3_bucket_public_access_block" "frontend_ui_public_access_block" {
-  bucket = aws_s3_bucket.frontend_ui.id
-  block_public_acls       = false
+  bucket                  = aws_s3_bucket.frontend_ui.id
+  block_public_acls       = true
+  ignore_public_acls      = true
   block_public_policy     = false
-  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-# Bucket Policy for public read access to the S3 objects
+# This policy grants read-only access to all objects in the bucket
 resource "aws_s3_bucket_policy" "frontend_ui_policy" {
   bucket = aws_s3_bucket.frontend_ui.id
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Sid = "PublicReadGetObject",
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = [
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": [
           "${aws_s3_bucket.frontend_ui.arn}/*",
         ]
       }
